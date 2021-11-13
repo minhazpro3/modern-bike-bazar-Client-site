638abd47995c3efe07cea77f78adf942461da5ce
minhazpro3 <minhazfr8@gmail.com>
2021-11-13 11:09:00 +0600

    update

--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import DashboardDetails from '../DashboardDetails/DashboardDetails';
+
 import {
-    
     Switch,
     Route,
     useRouteMatch
